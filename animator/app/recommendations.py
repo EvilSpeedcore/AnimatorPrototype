d90258b66252499459d6a3581b4122c420e6f0cd
EvilSpeedcore <fantafan95@gmail.com>
2018-11-11 19:15:10 +0300

    clean up base.html

--- animator/app/recommendations.py
+++ animator/app/recommendations.py
@@ -2,10 +2,12 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
+from app.auth import login_required
 
 bp = Blueprint('recommendations', __name__)
 
 
 @bp.route('/recommendations', methods=('GET', 'POST'))
+@login_required
 def show_recommendations():
-    return render_template('recommendations/show.html')
+    return render_template('recommendations/recommendations.html')
