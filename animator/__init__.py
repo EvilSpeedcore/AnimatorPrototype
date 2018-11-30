2815a49037c2fa25fe7f0890962a424f4287e1d6
Alexander Svoykin <Alexander.Svoykin@ascom.com>
2018-11-30 15:26:13 +0300

    hehe

--- animator/__init__.py
+++ animator/__init__.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 
+from celery import Celery
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
@@ -10,6 +11,23 @@ asyncio.set_event_loop(loop)
 from . import auth, anilist, db, prediction, recommendations
 
 
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+
 def create_app(test_config=None):
     """Create and configure the app."""
     app = Flask(__name__, instance_relative_config=True)
@@ -17,7 +35,10 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'anime.sqlite'),
+        CELERY_BROKER_URL='redis://localhost:6379',
+        CELERY_RESULT_BACKEND='redis://localhost:6379'
     )
+    celery = make_celery(app)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
