3689e5d255d7fe6c154f0f103ceac0eb47e17cb4
EvilSpeedcore <fantafan95@gmail.com>
2018-12-01 20:32:43 +0300

    change delete button id to anime title

--- animator/recommendations.py
+++ animator/recommendations.py
@@ -12,40 +12,34 @@ bp = Blueprint('recommendations', __name__)
 loop = asyncio.get_event_loop()
 
 
+def get_user_recommendations():
+    recommendations = DBController.query(
+        loop,
+        """
+        SELECT r.title, r.anime_type, r.episodes, r.studio, r.src, r.genre, r.score, r.synopsis, r.image_url, r.url
+        FROM recommendations r
+        WHERE r.profile_id = ?
+        """,
+        (session.get('user_id'), ), is_one=False)
+    return recommendations
+
+
 @bp.route('/recommendations', methods=('GET', 'POST'))
 @login_required
 def show_recommendations():
     #  TODO: Add URL to title.
-    recommendations = DBController.query(
-        loop,
-        """
-        SELECT r.title, r.anime_type, r.episodes, r.studio, r.src, r.genre, r.score, r.synopsis, r.image_url, r.url
-        FROM recommendations r
-        WHERE r.profile_id = ?
-        """,
-        (session.get('user_id'), ), is_one=False)
+    recommendations = get_user_recommendations()
     return render_template('recommendations/recommendations.html', recommendations=recommendations)
 
 
-@bp.route('/delete/<int:row_id>', methods=('GET', 'POST'))
+@bp.route('/delete', methods=('GET', 'POST'))
 @login_required
-def test(row_id):
-    print(row_id)
+def test():
     DBController.update(loop,
                         """
                         DELETE FROM recommendations
-                        WHERE id = ?
+                        WHERE title = ?
                         """,
-                        (row_id, ))
-    DBController.update(loop, 'DBCC CHECKIDENT (recommendation, RESEED, 1)')
-
-    recommendations = DBController.query(
-        loop,
-        """
-        SELECT r.title, r.anime_type, r.episodes, r.studio, r.src, r.genre, r.score, r.synopsis, r.image_url, r.url
-        FROM recommendations r
-        WHERE r.profile_id = ?
-        """,
-        (session.get('user_id'), ), is_one=False)
-    print(recommendations)
+                        (request.args.get('row_id'), ))
+    recommendations = get_user_recommendations()
     return render_template('recommendations/recommendations.html', recommendations=recommendations)
