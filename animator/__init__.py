b8efb5fbc22e6d3b6ccab6b83319a38a7b14d681
EvilSpeedcore <fantafan95@gmail.com>
2018-12-16 23:50:23 +0300

    add db initialization

--- animator/__init__.py
+++ animator/__init__.py
@@ -13,6 +13,7 @@ from . import auth, anilist, db, prediction, recommendations
 def create_app(test_config=None):
     """Create and configure the app."""
     app = Flask(__name__, instance_relative_config=True)
+    db.init_db()
     bootstrap = Bootstrap(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
