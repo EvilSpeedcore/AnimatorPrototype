9aca9d35cdda9ef0f492b4b8521d8052c56983e9
Alexander Svoykin <Alexander.Svoykin@ascom.com>
2018-12-17 15:46:37 +0300

    asd

--- animator/db.py
+++ animator/db.py
@@ -55,6 +55,7 @@ def init_db():
     db = sqlite3.connect(current_app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES)
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
+    db.close()
 
 
 @click.command('initdb')
