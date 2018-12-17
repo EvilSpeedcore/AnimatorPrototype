a4670e74be4cb57d4eee704c384c15f809ca7710
Alexander Svoykin <Alexander.Svoykin@ascom.com>
2018-12-17 16:20:02 +0300

    dasdas

--- animator/__init__.py
+++ animator/__init__.py
@@ -32,6 +32,8 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    db.init_app(app)
+
     app.register_blueprint(auth.bp)
     app.register_blueprint(anilist.bp)
     app.register_blueprint(prediction.bp)
