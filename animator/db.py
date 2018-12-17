c8f8f618e3c03740d75d46816e8567dd5dbc0453
Alexander Svoykin <Alexander.Svoykin@ascom.com>
2018-12-17 16:14:03 +0300

    123

--- animator/db.py
+++ animator/db.py
@@ -6,9 +6,9 @@ from flask.cli import with_appcontext
 import aiosqlite
 
 
-def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
+#def init_app(app):
+#    app.teardown_appcontext(close_db)
+#    app.cli.add_command(init_db_command)
 
 
 class DBController:
