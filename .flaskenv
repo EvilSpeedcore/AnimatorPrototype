57c823a8b3dc3090f0fe8d9961304741850455b6
EvilSpeedcore <fantafan95@gmail.com>
2018-12-23 00:14:38 +0300

    update config.py

--- .flaskenv
+++ .flaskenv
@@ -1,4 +1,4 @@
 FLASK_APP=animator
 FLASK_ENV=development
 APP_SETTINGS="config.DevelopmentConfig"
-DATABASE_URL="postgresql://localhost/anime"
+DATABASE_URL='postgresql://postgres:drubjack123@localhost/anime'
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'molingas'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:drubjack123@localhost/anime'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
