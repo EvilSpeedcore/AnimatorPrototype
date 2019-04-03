27c53f6e10fc10c8a1ea2e479a6b4394cbcfb546
EvilSpeedcore <fantafan95@gmail.com>
2019-04-03 12:53:26 +0300

    add age, country fields to registration, update login/register css

--- animator/models/models.py
+++ animator/models/models.py
@@ -7,6 +7,8 @@ class Siteuser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(), unique=True, nullable=False)
     password = db.Column(db.String())
+    age = db.Column(db.Integer())
+    country = db.Column(db.String())
 
     def __repr__(self):
         return '<Siteuser {}>'.format(self.username)
