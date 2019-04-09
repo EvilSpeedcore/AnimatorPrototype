22bbf77976579b7937f26d344196dd491ca6d02c
EvilSpeedcore <fantafan95@gmail.com>
2019-04-09 16:31:17 +0300

    add popular tab

--- animator/controllers/recommendations.py
+++ animator/controllers/recommendations.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 from flask import (
     Blueprint, render_template, request, session, redirect, url_for, g
@@ -8,7 +9,7 @@ from sqlalchemy import inspect
 
 from animator import db
 from animator.controllers.auth import login_required
-from animator.models.models import Profile, Recommendations, TopAnime, Statistics
+from animator.models.models import Profile, Recommendations, TopAnime, Statistics, Siteuser
 
 
 bp = Blueprint('recommendations', __name__)
@@ -95,3 +96,25 @@ def add_to_list():
     db.session.delete(recommendation)
     db.session.commit()
     return redirect(url_for('recommendations.show_recommendations'))
+
+
+@bp.route('/popular', methods=('GET', 'POST'))
+@login_required
+def recommendation_by_age():
+    if request.method == 'GET':
+        users_and_profiles = db.session.query(Siteuser, Profile). \
+                filter(Siteuser.id == Profile.profile_id). \
+                filter(Siteuser.privilege == 0). \
+                filter(Siteuser.age == g.user.age). \
+                filter(Siteuser.username != g.user.username). \
+                all()
+        if not users_and_profiles:
+            return render_template('recommendations/recommendation_by_age.html')
+        else:
+            user, profile = random.choice(users_and_profiles)
+            anime_list = json.loads(profile.list)
+            favourites = [value for value in zip(*anime_list.values()) if value[-1] >= 8]
+            recommendations = random.sample(favourites, 5) if len(favourites) >= 5 else favourites
+            fields = ('Title', 'Type', 'Episodes', 'Studio', 'Source', 'Genre', 'Score')
+            recommendations = [dict(zip(fields, r)) for r in recommendations]
+        return render_template('recommendations/recommendation_by_age.html', recommendations=recommendations)
